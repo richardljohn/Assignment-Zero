c940472cd6b1dcfb78084227853e92f9f758e70e
richardljohn <richardljohn@gmail.com>
2020-12-31 00:31:36 -0500

    Assignment 0

--- 10-sumOfNumsWithinARange/sumOfNumsWithinARange.js
+++ 10-sumOfNumsWithinARange/sumOfNumsWithinARange.js
@@ -1,6 +1,14 @@
 function sumOfNumsWithinARange(nums, start, end) {
-  // Insert code here;
+  var count = 0;
+  for(var i = 0; i < nums.length; i++) {
+    if((nums[i] >= start) && (nums[i] <= end)){
+      count++;
+    }
+  }
+  return count;
 }
+
+console.log(sumOfNumsWithinARange([2,4,5,6,23], 4, 6));
 
 // Do not edit this line;
 module.exports = sumOfNumsWithinARange;
--- 11-sumOfMinimumAndMaximum/sumOfMinimumAndMaximum.js
+++ 11-sumOfMinimumAndMaximum/sumOfMinimumAndMaximum.js
@@ -1,5 +1,15 @@
 function sumOfMinimumAndMaximum(nums) {
-  // Insert code here;
+  var max = nums[0];
+  var min = nums[0];
+  for(var i = 0; i < nums.length; i++){
+    if(max < nums[i]){
+      max = nums[i];
+    }
+    if(min > nums[i]){
+      max = nums[i];
+    }
+  }
+  return max + min;
 }
 
 // Do not edit this line;
