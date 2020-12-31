148ab2d2db56a56842bb186a28574b3b923f9354
richardljohn <richardljohn@gmail.com>
2020-12-31 18:32:13 -0500

    Assignment 0

--- 16-productOfAnyAmountOfNumbers/productOfAnyAmountOfNumbers.js
+++ 16-productOfAnyAmountOfNumbers/productOfAnyAmountOfNumbers.js
@@ -1,5 +1,17 @@
 function productOfAnyAmountOfNumbers(...args) {
-  // Insert code here;
+  var product = 1;
+  for(var i = 0; i < args.length; i++){
+    if(Array.isArray(args[i]) === true){
+      var a = args[i];
+      for(var j = 0; j < a.length; j++){
+        product *= a[j];
+      }
+    }
+    else {
+      product *= args[i];
+    }
+  }
+  return product;
 }
 
 // Do not edit this line;
