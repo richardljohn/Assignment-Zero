31e680feea7ca8ce14065a8649c1e814914412c7
richardljohn <richardljohn@gmail.com>
2020-12-31 01:02:43 -0500

    Assignment 0

--- 13-isPalindrome/isPalindrome.js
+++ 13-isPalindrome/isPalindrome.js
@@ -1,5 +1,12 @@
 function isPalindrome(word) {
-  // Insert code here;
+  var stack = new Stack();
+  var reverse = "";
+  for(var i = 0; i < word.length; i++){
+    stack.push(word[i]);
+  }
+  for(var j = 0; j < word.length; j++){
+    stack.
+  }
 }
 
 // Do not edit this line;
