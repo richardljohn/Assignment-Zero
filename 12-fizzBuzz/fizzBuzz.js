9bf951c9dc496f5b4614f470a54f97f098b8937b
richardljohn <richardljohn@gmail.com>
2020-12-31 00:53:16 -0500

    Assignment 0

--- 12-fizzBuzz/fizzBuzz.js
+++ 12-fizzBuzz/fizzBuzz.js
@@ -1,6 +1,23 @@
 function fizzBuzz(start, end) {
-  // Insert code here;
+  var arr = [];
+  for(start; start <= end; start++){
+    if((start % 3 == 0) && (start % 5 == 0)){
+      arr[start] = "FizzBuzz";
+    }
+    else if(start % 3 == 0){
+      arr[start] = "Fizz";
+    }
+    else if(start % 5 == 0) {
+      arr[start] = "Buzz";
+    }
+    else {
+      arr[start] = start;
+    }
+  }
+  return arr;
 }
+
+console.log(fizzBuzz(1, 15));
 
 // Do not edit this line;
 module.exports = fizzBuzz;
