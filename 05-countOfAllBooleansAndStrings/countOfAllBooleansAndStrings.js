2436af3effb3b0d3c0cd930bb8b423c2d9128388
richardljohn <richardljohn@gmail.com>
2020-12-30 23:45:55 -0500

    More of Assignment 0

--- 05-countOfAllBooleansAndStrings/countOfAllBooleansAndStrings.js
+++ 05-countOfAllBooleansAndStrings/countOfAllBooleansAndStrings.js
@@ -1,6 +1,14 @@
 function countOfAllBooleansAndStrings(arr) {
-  // Insert code here;
+  var count = 0;
+  for(var i = 0; i < arr.length; i++){
+    if((typeof arr[i] == 'boolean') || (typeof arr[i] == 'string')){
+      count++;
+    }
+  }
+  return count;
 }
+
+console.log(countOfAllBooleansAndStrings(["hey", "there", "hello", "there", "world", false]));
 
 // Do not edit this line;
 module.exports = countOfAllBooleansAndStrings;
--- 06-countOfAllNumbersSmallerThanTarget/countOfAllNumbersSmallerThanTarget.js
+++ 06-countOfAllNumbersSmallerThanTarget/countOfAllNumbersSmallerThanTarget.js
@@ -1,6 +1,14 @@
 function countOfAllNumbersSmallerThanTarget(nums, target) {
-  // Insert code here;
+  var count = 0;
+  for(var i = 0; i < nums.length; i++){
+    if(nums[i] < target){
+      count++;
+    }
+  }
+  return count; 
 }
+
+console.log(countOfAllNumbersSmallerThanTarget([1,5,3,2,20,18], 18));
 
 // Do not edit this line;
 module.exports = countOfAllNumbersSmallerThanTarget;
