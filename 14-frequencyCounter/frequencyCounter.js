4fb08801df224df13a458ba7289c7593ad0f5957
richardljohn <richardljohn@gmail.com>
2020-12-31 16:28:20 -0500

    Assignment 0

--- 14-frequencyCounter/frequencyCounter.js
+++ 14-frequencyCounter/frequencyCounter.js
@@ -1,6 +1,17 @@
 function frequencyCounter(word) {
-  // Insert code here;
+  var w = new Object();
+  for(var i = 0; i < word.length; i++){
+    key = word.charAt(i);
+    if(w[key]){
+      w[key]++;
+    } else {
+      w[key] = 1;
+    }
+  }
+  return w; 
 }
+
+console.log(frequencyCounter("apple"));
 
 // Do not edit this line;
 module.exports = frequencyCounter;
