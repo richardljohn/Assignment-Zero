1e7e64273de34f069e152b5b26ebc0a35fd9a46a
richardljohn <richardljohn@gmail.com>
2020-12-31 19:24:56 -0500

    Assignment 0

--- 17-setUnionOfAnyAmountOfSets/setUnionOfAnyAmountOfSets.js
+++ 17-setUnionOfAnyAmountOfSets/setUnionOfAnyAmountOfSets.js
@@ -1,10 +1,14 @@
 function setUnionOfAnyAmountOfSets(...args) {
   let newSet = new Set();
   for(var i = 0; i < args.length; i++){
-    newSet.union(newSet, args[i]);
+    for(const elem of args[i]){
+      newSet.add(elem);
+    }
   }
   return newSet;
 }
+
+console.log(setUnionOfAnyAmountOfSets([1, 2, 4 ], [4, 5, 6 ], [ 6, 7, 8 ]));
 
 // Do not edit this line;
 module.exports = setUnionOfAnyAmountOfSets;
