ac2dfb27e2fefc91f16d72a06d0c16f5fd2c62f0
richardljohn <richardljohn@gmail.com>
2020-12-31 20:47:19 -0500

    Just a bit more left to go.

--- 19-binarySearch/binarySearch.js
+++ 19-binarySearch/binarySearch.js
@@ -3,10 +3,31 @@ class MySolution {
     this.flag = false; // this is optional to use;
   }
 
-  binarySearch(nums, target) {
-    // Insert code here;
+  // binarySearch(nums, target) {
+  //   let mid = (nums.length)/2;
+  //   if(nums.length = 1){
+  //     if(target === nums[0]){
+  //       return true;
+  //     } 
+  //   }
+  //   if(target === nums[mid]){
+  //     return true;
+  //   }
+  //   else if(target > nums[mid]){
+  //     return this.binarySearch(nums.splice(mid+1, nums.length), target);
+  //   }
+  //   else {
+  //     return this.binarySearch(nums.splice(0, mid), target);
+  //   }
+  // }
+  binarySearch(nums, target, start, end){
+    
   }
 }
+
+let solution = new MySolution();
+let arr = [1,2,3,4,5];
+console.log(solution.binarySearch(arr, 3, 0, arr.length));
 
 // Do not edit this line;
 let StudentSolution = MySolution;
