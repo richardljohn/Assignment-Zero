719706ffcdc2008454378aac80b802bfc90d8dc6
richardljohn <richardljohn@gmail.com>
2021-01-03 23:28:26 -0500

    Final Assignment 0

--- 19-binarySearch/binarySearch.js
+++ 19-binarySearch/binarySearch.js
@@ -2,44 +2,28 @@ class MySolution {
   constructor() {
     this.flag = false; // this is optional to use;
   }
-
-  // binarySearch(nums, target) {
-  //   let mid = (nums.length)/2;
-  //   if(nums.length = 1){
-  //     if(target === nums[0]){
-  //       return true;
-  //     } 
-  //   }
-  //   if(target === nums[mid]){
-  //     return true;
-  //   }
-  //   else if(target > nums[mid]){
-  //     return this.binarySearch(nums.splice(mid+1, nums.length), target);
-  //   }
-  //   else {
-  //     return this.binarySearch(nums.splice(0, mid), target);
-  //   }
-  // }
-  binarySearch(nums, target, start, end){
-    if(start > end){
+  binarySearch(nums, target){
+    if(nums.length === 0){
       return false;
     }
-    var mid = Math.floor((start+end)/2);
-    if(nums[mid] == target){
-      return true; 
-    }
-    else if(target > nums[mid]){
-      return this.binarySearch(nums, target, mid+1, end);
-    }
-    else if(target < nums[mid]){
-      return this.binarySearch(nums, target, start, mid-1);
+    else { 
+      var mid = Math.floor(nums.length/2);
+      if(nums[mid] === target){
+        return true; 
+      }
+      else if (nums[mid] > target) { 
+        return this.binarySearch(nums.slice(0, mid), target);
+      }
+      else {
+        return this.binarySearch(nums.slice(mid+1), target);
+      }
     }
   }
 }
 
 let solution = new MySolution();
 let arr = [1,2,3,4,5];
-console.log(solution.binarySearch(arr, 0, 0, arr.length));
+console.log(solution.binarySearch(arr, 9));
 
 // Do not edit this line;
 let StudentSolution = MySolution;
